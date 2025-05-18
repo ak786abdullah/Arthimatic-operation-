c6ba97d87403c4ccd458be3e8f3076d8240604de
muhammad abdullah <abdullahkhosa11223344@gmail.com>
2025-05-18 16:30:38 +0500

    Add files via upload

---
+++ arthimatic operation.py
@@ -0,0 +1,37 @@
+# task 1 (addition)
+x=7
+y=5
+# the value of x and y are integers 
+result=x+y # stored addition of x and y in result 
+print(f'7+5={result}') # out put=(7+5=12)
+# task 2 (subtraction) (y from x)
+result1=x-y 
+print(f'7-5={result1}') # out put =(7-5=2)
+# task3 multyply 
+result3=x*y 
+print(f"7*5={result3}") # out put=(7*5=35)
+# when we add, subtract and multply two or more  integers the result will also a integer
+# task 4 division 
+result4=x/y 
+print(f"7/5={result4}") # out put=(7/5=1.4)
+# floor division 
+result5=x//y
+print(f'7//5={result5}')# out put=(7//5=1) in floor division out put will be whole number (left side of dacimal)
+# modulus (reminder in division proccess)
+result6=x%y 
+print(f"7%5={result6}") # output=(7%5=2) means reminder =2
+# exponentiation (power )
+result7=7**5
+print(f"7**5={result7}") #out put =( 7**5=16807)
+# what will happen we use only value instead variable 
+# let check
+result8=7+5
+print(result8) # out put =12 
+# get the same results (outputs)
+# bounas challange 
+print(f""" addition:
+      7+5={result}
+subtraction:
+        7-5={result1}
+multiplication:
+        7*5={result3}""") # when we print variables more then 1 line  we use triple qoutation 
