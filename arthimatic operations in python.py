8473c86736ea84c51dbfb733012f1db1d6d7b83a
muhammad abdullah <abdullahkhosa11223344@gmail.com>
2025-05-18 16:31:41 +0500

    Add files via upload

---
+++ arthimatic operations in python.py
@@ -0,0 +1,27 @@
+#addition(+)
+a=5
+b=4.5
+result=a+b
+print(f'{a}+{b}={result}')
+#subtraction(-)
+a=7
+b=8.5 
+result=a-b
+print(f"{a} - {b}={result}") # it will take nearest value of variable 
+#multiplication(*)
+result=a*b
+print(f'{a}*{b}={result}')
+#division(/)
+result=a/b
+print(f"{a}/{b}={result}")
+#modulus (%) (reminder in division proccess)
+a=4
+b=5
+result =a%b
+print(f"{a}%{b}={result}")
+#floor division(//) when qoutient is in dacimal floor divison  terminate the dacimal
+result=a//b
+print(f"{a}//{b}={result}")
+#exponentiation(**) (power)
+result=a**b
+print(f"{a}**{b}={result}")
